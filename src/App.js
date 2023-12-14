195a732438b0329eb5c901ca7b3743dffcc1f960
lucky sharda <luckysharda1011@gmail.com>
2023-12-15 01:36:28 +0530

    signin/signup from && Login/header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import "./App.css";
+import Body from "./components/Body";
 
 function App() {
-  return <div className=" text-3xl font-bold">hello</div>;
+  return <Body />;
 }
-
 export default App;
