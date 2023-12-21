716f586cb62aed41428a786897dc579a95de21e1
lucky sharda <luckysharda1011@gmail.com>
2023-12-21 16:02:44 +0530

    making the GPT Responsive

--- src/components/GptSearch.js
+++ src/components/GptSearch.js
@@ -5,14 +5,16 @@ import { BG_URL } from "../utils/constants";
 
 const GptSearch = () => {
   return (
-    <div>
-      <div className="fixed -z-10">
+    <>
+      <div className="absolute -z-10">
         <img src={BG_URL} alt="logo" />
       </div>
 
-      <GptSearchBar />
-      <GptMovieSuggestions />
-    </div>
+      <div className="">
+        <GptSearchBar />
+        <GptMovieSuggestions />
+      </div>
+    </>
   );
 };
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -58,9 +58,9 @@ const Header = () => {
     dispatch(changeLanguage(e.target.value));
   };
   return (
-    <div className=" absolute w-full px-8 py-2 bg-gradient-to-b from-black z-10 flex justify-between">
-      <img className="w-44" src={LOGO} alt="logo" />
-      <div className="flex p-2">
+    <div className=" absolute w-full px-8 py-2 bg-gradient-to-b from-black z-10 flex flex-col md:flex-row justify-center">
+      <img className="w-44 mx-auto md:mx-0" src={LOGO} alt="logo" />
+      <div className="flex p-2 justify-between">
         {showGptSearch && (
           <select
             className="p-2 m-2 bg-gray-900 text-white"
@@ -81,7 +81,7 @@ const Header = () => {
           {showGptSearch ? "Homepage" : "GPT Search"}
         </button>
         <img
-          className="w-12 h-12"
+          className="hidden md:block w-12 h-12"
           src="https://occ-0-6247-2164.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABdpkabKqQAxyWzo6QW_ZnPz1IZLqlmNfK-t4L1VIeV1DY00JhLo_LMVFp936keDxj-V5UELAVJrU--iUUY2MaDxQSSO-0qw.png?r=e6e"
           alt="usericon"
         />
